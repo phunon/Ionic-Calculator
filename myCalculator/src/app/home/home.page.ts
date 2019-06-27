5162f4a488653c41492a385832d64a0261d6c383
Phunon Koysawat <phunon@kkumail.com>
2019-06-27 14:05:22 +0700

    v1
    Bug:
    - try to press '=' first the result is show undefined

--- myCalculator/src/app/home/home.page.ts
+++ myCalculator/src/app/home/home.page.ts
@@ -7,6 +7,37 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  calculate = "";
+  result = "";
+  
   constructor() {}
+
+  btnClicked(btn) {
+    console.log('CalculatorPage::btnClicked = ' + btn + " result: " + this.result );
+    var lastResult = "";
+    try {
+      if (btn == "C") {
+        this.calculate = "";
+      } else if (btn == "=") {  
+        this.calculate = eval(this.result);
+      } else if (btn == "%") {
+        this.calculate = eval(this.result+"/"+100);
+      } else if (btn == "backspace") {
+        this.calculate = eval(this.result.substr(0, this.result.length - 1));
+      } else {
+        this.calculate += btn;
+      }
+      try {
+        this.result = eval(this.calculate);
+        lastResult = this.result;
+      } catch {
+        this.result = lastResult;
+      }
+    } catch (e) {
+      this.calculate = ("Error");
+      console.log(e);
+    }
+    
+  }
 
 }
